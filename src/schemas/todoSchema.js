e298b3c5e0e036bc00bf361d303a0aae15418481
ZaynIkhlaq <zaynikhlaq@gmail.com>
2025-11-22 18:43:00 +0500

    finished TodoForm

--- src/schemas/todoSchema.js
+++ src/schemas/todoSchema.js
@@ -3,7 +3,7 @@ import { z } from "zod";
 // creating the scheme using zod
 export const todoSchema = z.object({
     id: z.number(), 
-    title: z.string(),
+    title: z.string().min(1, "Title is required"),
     completed:z.boolean().default(false),
 });
 
